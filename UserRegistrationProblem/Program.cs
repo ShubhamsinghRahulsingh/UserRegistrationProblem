af086013b29e61ef94ad57afba46e5cad8758332
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-11-29 20:04:25 +0530

    (Shubham)Add: Validating first name that starts with capital letter and has minimum 3 character in it

--- UserRegistrationProblem/Program.cs
+++ UserRegistrationProblem/Program.cs
@@ -5,7 +5,29 @@ namespace UserRegistrationProblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the User Registration Problems");         
+            PatternCheck check = new PatternCheck();
+            Console.WriteLine("Welcome to the User Registration Problems");
+            bool flag = true;
+            while(flag)
+            {
+                Console.WriteLine("------------------------------------------------------");
+                Console.WriteLine("Select from the below which you want to validate");
+                Console.WriteLine("1.First Name\n2.Exit");
+                Console.Write("Enter your choice: ");
+                int choice=Convert.ToInt32(Console.ReadLine());
+                switch(choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter your First name");
+                        string firstName=Console.ReadLine();
+                        check.ValidateFirstName(firstName);
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+
+                }
+            }
         }
     }
 }
